20d0820ae6981f2a48d5e27725bfaa031cca4dc8
pedrohnf <pedrohnf2014@gmail.com>
2019-03-27 17:51:21 -0300

    first commit

---
+++ src/main/java/com/pa2/milk/api/ServiceMilkApplication.java
@@ -0,0 +1,13 @@
+package com.pa2.milk.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceMilkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceMilkApplication.class, args);
+	}
+
+}
