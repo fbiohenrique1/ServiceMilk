d6fe980dc07280a03bedade792b5231ec8c75aa9
pedrohnf <pedrohnf2014@gmail.com>
2019-05-31 18:23:12 -0300

    Ajustes nos Enuns: EnumOrigemLeite, EnumProdutos, EnumEspecie e EnumAnalisesSolicitadas

--- src/main/java/com/pa2/milk/api/model/enums/EnumEspecie.java
+++ src/main/java/com/pa2/milk/api/model/enums/EnumEspecie.java
@@ -1,5 +1,5 @@
 package com.pa2.milk.api.model.enums;
 
 public enum EnumEspecie {
-	CAPRINO, OUVINO, BOVINO, BOBALINO, ECLINOE, ASININO;
+	CAPRINO, OVINO, BOVINO, BUBALINO, EQUINO, ASILINO;
 }
--- src/main/java/com/pa2/milk/api/model/enums/EnumProdutos.java
+++ src/main/java/com/pa2/milk/api/model/enums/EnumProdutos.java
@@ -1,5 +1,5 @@
 package com.pa2.milk.api.model.enums;
 
 public enum EnumProdutos {
-	CREME_30_GORDURA, SORO, CREME_45_GORDURA
+	CREME_30_GORDURA, SORO, CREME_45_GORDURA, BEBIDA_LACTEA, IORGUTE, SORVETE, QUEIJO, LEITE
 }
