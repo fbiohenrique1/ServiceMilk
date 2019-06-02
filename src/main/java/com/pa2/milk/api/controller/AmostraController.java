3253da30f79b67941abe0ed098b2d48ef98aab82
pedrohnf <pedrohnf2014@gmail.com>
2019-06-02 20:05:01 -0300

    Criação Lista de QrCode de acordo com a Analise (Incompleto)

--- src/main/java/com/pa2/milk/api/controller/AmostraController.java
+++ src/main/java/com/pa2/milk/api/controller/AmostraController.java
@@ -6,16 +6,24 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.zxing.WriterException;
+import com.pa2.milk.api.model.Amostra;
+import com.pa2.milk.api.repository.AmostraRepository;
+import com.pa2.milk.api.repository.AnaliseRepository;
 
 import net.glxn.qrgen.core.image.ImageType;
+import net.glxn.qrgen.core.vcard.VCard;
 import net.glxn.qrgen.javase.QRCode;
 
 @RestController
@@ -24,24 +32,42 @@ import net.glxn.qrgen.javase.QRCode;
 public class AmostraController {
 //O crud de amostra envolve a busca pelo id da solicitação e o id de analise
 
-	@GetMapping(value = "/text")
-	public void getQRCodeImage(@RequestParam("text") String text) throws WriterException, IOException {
+	@Autowired
+	private AmostraRepository amostraRepositorio;
 
-		ByteArrayOutputStream bout = QRCode.from(text).withSize(250, 250).to(ImageType.PNG).stream();
+	@Autowired
+	private AnaliseRepository analiseRepositorio;
 
-		try {
-			File file = new File("C:\\Users\\phnf2\\Pictures\\teste\\qr_code.png");
-			OutputStream out = new FileOutputStream(file);
-			bout.writeTo(out);
-			out.flush();
-			out.close();
+	@GetMapping(value = "{id}/QrCode")
+	public void getQRCodeImage(@PathVariable("idAnalise") Integer idAnalise) throws WriterException, IOException {
 
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
+		List<Amostra> a = this.amostraRepositorio.findByAnalise(idAnalise);
+
+		for (int i = 0; i < a.size(); i++) {
+
+			 VCard vCard = new VCard();
+			 vCard.setName("Amostra "+String.valueOf(a.get(i).getId()));
+			 vCard.setCompany(String.valueOf(a.get(i).getDataColeta()));
+		     vCard.setTitle(String.valueOf(a.get(i).getNumeroAmostra()) );
+		     vCard.setEmail(String.valueOf(a.get(i).getObservacao()));
+		     vCard.setWebsite(String.valueOf(a.get(i).getQrCode()));
+		        
+			ByteArrayOutputStream bout = QRCode.from(vCard).withSize(250, 250).to(ImageType.PNG).stream();
+
+			try {
+				File file = new File("C:\\Users\\phnf2\\Pictures\\teste\\qr_code("+i+").png");
+				OutputStream out = new FileOutputStream(file);
+				bout.writeTo(out);
+				out.flush();
+				out.close();
+
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
 		}
-
 	}
 
 }
--- src/main/java/com/pa2/milk/api/repository/AmostraRepository.java
+++ src/main/java/com/pa2/milk/api/repository/AmostraRepository.java
@@ -1,7 +1,19 @@
 package com.pa2.milk.api.repository;
 
-import com.pa2.milk.api.model.Amostra;
+import java.util.List;
 
+import javax.persistence.NamedQuery;
+
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.pa2.milk.api.model.Amostra;
+import com.pa2.milk.api.model.Analise;
+
+@Transactional(readOnly = true)
+@NamedQuery(name = "AmostraRepository.findByAmostraId", query = "SELECT a FROM Amostra a WHERE a.analise.id = :analiseId")
 public interface AmostraRepository extends GenericRepository<Amostra, Integer> {
+
+	List<Amostra> findByAnalise(@Param("analiseId") Integer analiseId);
 
 }
