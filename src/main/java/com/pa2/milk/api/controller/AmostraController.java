ac96e303f2c36663002734b14be196da938ba81c
pedrohnf <pedrohnf2014@gmail.com>
2019-06-05 22:02:08 -0300

    Criando o Listar Amostras por Analises para Web gerar os QrCodes

--- src/main/java/com/pa2/milk/api/controller/AmostraController.java
+++ src/main/java/com/pa2/milk/api/controller/AmostraController.java
@@ -1,30 +1,16 @@
 package com.pa2.milk.api.controller;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.google.zxing.WriterException;
 import com.pa2.milk.api.model.Amostra;
 import com.pa2.milk.api.repository.AmostraRepository;
-import com.pa2.milk.api.repository.AnaliseRepository;
-
-import net.glxn.qrgen.core.image.ImageType;
-import net.glxn.qrgen.core.vcard.VCard;
-import net.glxn.qrgen.javase.QRCode;
 
 @RestController
 @RequestMapping(value = "/amostra")
@@ -35,37 +21,11 @@ public class AmostraController {
 	@Autowired
 	private AmostraRepository amostraRepositorio;
 
-	@Autowired
-	private AnaliseRepository analiseRepositorio;
-
 	@GetMapping(value = "{analiseId}/QrCode")
-	public void getQRCodeImage(@PathVariable("analiseId") Integer analiseId) throws WriterException, IOException {
+	public List<Amostra> listarAmostrasPorAnalise(@PathVariable("analiseId") Integer analiseId) {
 
-		// List<Amostra> a = this.amostraRepositorio.findByAnaliseId(analiseId);
+		List<Amostra> amostras = this.amostraRepositorio.findByAnaliseId(analiseId);
 
-		Optional<Amostra> a = this.amostraRepositorio.findById(analiseId);
-		
-		VCard vCard = new VCard();
-		vCard.setName("Amostra " + String.valueOf(a.get().getId()));
-		vCard.setCompany(String.valueOf(a.get().getDataColeta()));
-		vCard.setTitle(String.valueOf(a.get().getNumeroAmostra()));
-		vCard.setEmail(String.valueOf(a.get().getObservacao()));
-		vCard.setWebsite(String.valueOf(a.get().getQrCode()));
-
-		ByteArrayOutputStream bout = QRCode.from(vCard).withSize(250, 250).to(ImageType.PNG).stream();
-
-		try {
-			File file = new File("C:\\Users\\phnf2\\Pictures\\teste\\qr_code.png");
-			OutputStream out = new FileOutputStream(file);
-			bout.writeTo(out);
-
-			out.flush();
-			out.close();
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		return amostras;
 	}
 }
